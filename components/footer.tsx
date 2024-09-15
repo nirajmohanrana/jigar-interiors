ab5de70de73b039c0498bc95d83542f50b4848b1
Niraj Rana <71956273+nirajmohanrana@users.noreply.github.com>
2024-09-15 13:51:05 +0530

    ssr fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,26 +1,30 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import { IoLocationSharp } from "react-icons/io5";
 import { IoIosCall } from "react-icons/io";
 import { IoIosMail } from "react-icons/io";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaFacebook } from "react-icons/fa";
 import Link from "next/link";
-import { Popup } from "react-leaflet";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
+// Dynamically import MapContainer to prevent SSR issues
 const MapContainer = dynamic(
   () => import("react-leaflet").then((mod) => mod.MapContainer),
-  { ssr: false }
-);
-const Marker = dynamic(
-  () => import("react-leaflet").then((mod) => mod.Marker),
   { ssr: false }
 );
 const TileLayer = dynamic(
   () => import("react-leaflet").then((mod) => mod.TileLayer),
   { ssr: false }
 );
+const Marker = dynamic(
+  () => import("react-leaflet").then((mod) => mod.Marker),
+  { ssr: false }
+);
+const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), {
+  ssr: false,
+});
 
 const footerLinks = [
   {
@@ -51,6 +55,12 @@ const footerLinks = [
 ];
 
 const Footer = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <div className="bg-foreground text-background px-4 py-8">
       <div className="w-full mx-auto md:max-w-6xl">
@@ -69,32 +79,34 @@ const Footer = () => {
           </div>
 
           <div className="w-full md:w-1/2 h-64">
-            <MapContainer
-              center={[19.433001, 72.862427]}
-              zoom={15}
-              attributionControl={false}
-              scrollWheelZoom={false}
-              className="w-full h-full"
-            >
-              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-              <Marker position={[19.433001, 72.862427]}>
-                <Popup>
-                  <Link
-                    href="https://www.google.com/maps/place/VITHAL+HARI+TOWER/@19.4551291,72.8064361,154a,35y,45t/data=!3m1!1e3!4m6!3m5!1s0x3be7a91b5242b2fb:0x5dbb8496715e654c!8m2!3d19.4564704!4d72.8070546!16s%2Fg%2F11vkn204tt?entry=ttu"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    <p className="text-center">
-                      Shop no 7 Vithal Hari tower, Virar (W)
-                      <br />
-                      <span className="text-xs opacity-50">
-                        View on Google Maps
-                      </span>
-                    </p>
-                  </Link>
-                </Popup>
-              </Marker>
-            </MapContainer>
+            {isMounted && (
+              <MapContainer
+                center={[19.433001, 72.862427]}
+                zoom={15}
+                attributionControl={false}
+                scrollWheelZoom={false}
+                className="w-full h-full"
+              >
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <Marker position={[19.433001, 72.862427]}>
+                  <Popup>
+                    <Link
+                      href="https://www.google.com/maps/place/VITHAL+HARI+TOWER/@19.4551291,72.8064361,154a,35y,45t/data=!3m1!1e3!4m6!3m5!1s0x3be7a91b5242b2fb:0x5dbb8496715e654c!8m2!3d19.4564704!4d72.8070546!16s%2Fg%2F11vkn204tt?entry=ttu"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                    >
+                      <p className="text-center">
+                        Shop no 7 Vithal Hari tower, Virar (W)
+                        <br />
+                        <span className="text-xs opacity-50">
+                          View on Google Maps
+                        </span>
+                      </p>
+                    </Link>
+                  </Popup>
+                </Marker>
+              </MapContainer>
+            )}
           </div>
         </div>
       </div>
